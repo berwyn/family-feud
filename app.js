69ecbe3d7d3fa6c72db63d4e229d46f77f52f942
berwyn <berwyn.codeweaver@gmail.com>
2014-08-09 19:48:39 -0600

    Basic layout and core structure

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    sass    = require('node-sass'),
+    app     = express(),
+    port    = process.env.PORT || 3000;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(sass.middleware({
+    src: __dirname + '/style',
+    dest: __dirname + '/public',
+    debug: true,
+    outputStyle: 'compressed'
+}));
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/login', function(req, res) {
+
+});
+
+app.post('/login', function(req, res) {
+
+});
+
+app.get('/answer', function(req, res) {
+
+});
+
+app.post('/answer', function(req, res) {
+
+});
+
+var server = app.listen(port, function() {
+    console.log('Listening on port %d', server.address().port);
+});
