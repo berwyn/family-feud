f3bc8b1eaaa2d3e5d0e3d19af945981da3f9e81e
berwyn <berwyn.codeweaver@gmail.com>
2014-08-09 21:26:27 -0600

    Answer spacing and borders

--- js/GameDirective.js
+++ js/GameDirective.js
@@ -5,7 +5,7 @@ function GameDirective() {
             '<div class="game row">',
                 '<div class="question">',
                     '<h1 class="col-xs-12 title">{{game.currentQuestion.text}}</h1>',
-                    '<span class="answer col-xs-4 col-xs-offset-1" ng-repeat="answer in game.currentQuestion.answers">',
+                    '<span class="answer col-xs-4" ng-class="{\'col-xs-offset-1\':$index % 2 === 0, \'col-xs-offset-2\':$index % 2 === 1}" ng-repeat="answer in game.currentQuestion.answers">',
                         '{{ answer.text }}',
                     '</span>',
                 '</div>',
